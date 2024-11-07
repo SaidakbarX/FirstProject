020934e1a88aeec45d9fe1ef2282591a32ad9891
SaidakbarX <khuyumkhujayev6937>
2024-11-07 22:48:05 +0300

    said

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("enter number please");
+            var number = Convert.ToInt32(Console.ReadLine());
+            var thors = number / 1000;
+            var hondr = number % 1000 / 100;
+            var ten =number % 1000 % 100 /10;
+            var one = number % 10;
+            var res = thors + ten + hondr + one;
+            Console.WriteLine(res);
+            
         }
     }
 }
--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4bdcdf6cb7ec20e3953c76163660a8baa6903c")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
